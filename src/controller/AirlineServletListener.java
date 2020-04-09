79f48cd6f243def3a6b01a13ab8fdc4bc4b103ff
elizavetapavliv <pavliv.liza@yandex.ru>
2020-04-09 22:34:53 +0300

    Properties

--- src/controller/AirlineServletListener.java
+++ src/controller/AirlineServletListener.java
@@ -1,5 +1,9 @@
 package controller;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.ResourceBundle;
+
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 import javax.servlet.ServletContextEvent;
@@ -12,7 +16,18 @@ public class AirlineServletListener implements ServletContextListener {
 
 	@Override
     public void contextInitialized(ServletContextEvent event) {
-		EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("Airline");
+	
+		ResourceBundle resource = ResourceBundle.getBundle("database");
+		String url = resource.getString("url");
+		String driver = resource.getString("driver");
+		String user = resource.getString("user");
+		String pass = resource.getString("password");
+		Map map = new HashMap();
+		map.put("javax.persistence.jdbc.url", url);
+		map.put("javax.persistence.jdbc.user", user);
+		map.put("javax.persistence.jdbc.driver", driver);
+		map.put("javax.persistence.jdbc.password", pass);
+		EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("Airline", map);
 		event.getServletContext().setAttribute("emf", entityManagerFactory);
     }
 	
