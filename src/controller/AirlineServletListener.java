8e94992d6288b3e30c3375054fa954194fc2c002
elizavetapavliv <pavliv.liza@yandex.ru>
2020-04-10 13:11:35 +0300

    Properties file

--- src/controller/AirlineServletListener.java
+++ src/controller/AirlineServletListener.java
@@ -18,20 +18,19 @@ import org.eclipse.persistence.internal.security.JCEEncryptor;
 public class AirlineServletListener implements ServletContextListener {
 
 	@Override
-    public void contextInitialized(ServletContextEvent event) {
-	
-/*		ResourceBundle resource = ResourceBundle.getBundle("database");
+    public void contextInitialized(ServletContextEvent event) {	
+		ResourceBundle resource = ResourceBundle.getBundle("database");
 		String url = resource.getString("url");
 		String driver = resource.getString("driver");
 		String user = resource.getString("user");
-		String pass = resource.getString("password");*/
+		String pass = resource.getString("password");
 		Map map = new HashMap();
-		/*map.put("javax.persistence.jdbc.url",url);
+		map.put("javax.persistence.jdbc.url",url);
 		map.put("javax.persistence.jdbc.user", user);
-		map.put("javax.persistence.jdbc.driver", driver);*/
-		map.put("javax.persistence.jdbc.url","jdbc:postgresql://ec2-54-228-251-117.eu-west-1.compute.amazonaws.com:5432/data9uq0cgo999?sslmode=require");
+		map.put("javax.persistence.jdbc.driver", driver);
+	/*	map.put("javax.persistence.jdbc.url","jdbc:postgresql://ec2-54-228-251-117.eu-west-1.compute.amazonaws.com:5432/data9uq0cgo999?sslmode=require");
 		map.put("javax.persistence.jdbc.user", "apugtfccgqifho");
-		map.put("javax.persistence.jdbc.driver", "org.postgresql.Driver");
+		map.put("javax.persistence.jdbc.driver", "org.postgresql.Driver");*/
 		String a = "A: ";
 		try {
 			map.put("javax.persistence.jdbc.password", new JCEEncryptor().encryptPassword("8c48dac59ebe215e4d67521b3758302a29bf6a6ac07eb969472fa64b12cb81dc"));
