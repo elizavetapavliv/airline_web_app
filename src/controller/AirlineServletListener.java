63f74fcbff4498e6fbb7b55fae0aabc40505d0c5
elizavetapavliv <pavliv.liza@yandex.ru>
2020-04-23 19:15:02 +0300

    Persistence

--- src/controller/AirlineServletListener.java
+++ src/controller/AirlineServletListener.java
@@ -1,25 +1,18 @@
 package controller;
 
-import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
-import javax.persistence.PersistenceContext;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 import javax.servlet.annotation.WebListener;
 
 /**Airline servlet listener*/
 @WebListener
-public class AirlineServletListener implements ServletContextListener {
-
-	 @PersistenceContext(unitName = "Airline")  
-     private EntityManager ctxManager;  
+public class AirlineServletListener implements ServletContextListener { 
 	
 	@Override
     public void contextInitialized(ServletContextEvent event) {	
-		event.getServletContext().log("CTX: " + ctxManager);
 		EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("Airline");
-		event.getServletContext().log("CTXEMF: " + entityManagerFactory);
 		event.getServletContext().setAttribute("emf", entityManagerFactory);
     }
 	
